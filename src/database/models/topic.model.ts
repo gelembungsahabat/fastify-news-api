27f0679cb291b0ddb20456ff851d4d6043354033
gelembungsahabat <wildanrgssport@gmail.com>
2021-08-24 20:42:03 +0700

    fixing topic model

--- src/database/models/topic.model.ts
+++ src/database/models/topic.model.ts
@@ -2,19 +2,20 @@ import { Model } from 'objection';
 
 import { TableName } from '../tablename';
 import { BaseModel } from './base.model';
-// import { TopicModel } from './topic.model';
+import { NewsModel } from './news.model';
 
 export class TopicModel extends BaseModel {
   topic_name!: string;
   created_at!: Date;
   updated_at!: Date;
 
+  news!: NewsModel;
   static tableName = TableName.TOPIC;
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   static relationMappings = (): any => ({
-    topic: {
-      modelClass: TopicModel,
+    news: {
+      modelClass: NewsModel,
       relation: Model.ManyToManyRelation,
       join: {
         from: `${TableName.TOPIC}.id`,
